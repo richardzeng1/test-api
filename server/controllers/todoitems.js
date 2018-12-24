217f16f91fc179d73cbe33b825f995573bb067d1
richard zeng <richardzeng337@gmail.com>
2018-12-24 16:49:44 -0500

    completed api

--- server/controllers/todoitems.js
+++ server/controllers/todoitems.js
@@ -7,5 +7,45 @@ module.exports = {
             todoId: req.params.todoId
         }).then(todoItem => res.status(201).send(todoItem))
         .catch(error => res.status(400).send(error))
+    },
+
+    update(req, res){
+        return TodoItem.find({
+            where:{
+                id: req.params.todoItemId,
+                todoId: req.params.todoId
+            }
+        }).then(todoItem => {
+            if (!todoItem){
+                return res.status(404).send({
+                    message:"Item not found"
+                })
+            }
+            return todoItem.update({
+                content: req.body.content || todoItem.content,
+                complete: req.body.complete || todoItem.complete
+            }).then(updatedTodoItem => res.status(202).send(updatedTodoItem))
+            .catch(error => res.status(400).send(error))
+        })
+    },
+
+    destroy(req, res){
+        return TodoItem.find({
+            where:{
+                id: req.params.todoItemId,
+                todoId: req.params.todoId
+            }
+        }).then(todoItem => {
+            if (!todoItem){
+                return res.status(404).send({
+                    message:"Item not found"
+                })
+            }
+            return todoItem.destroy()
+            .then(()=>res.status(200).send({
+                message:"Item deleted"
+            }))
+            .catch(error => res.status(400).send(error))
+        })
     }
 }
