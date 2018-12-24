6e23bf19d814824c23c2945b45d77f2c610f8d02
richard zeng <richardzeng337@gmail.com>
2018-12-24 16:54:08 -0500

    Added routes for todo item

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,19 @@ module.exports = (app) => {
     }))
 
     app.post("/api/todos/:todoId/items", todoItemControllers.create);
+    app.put("/api/todos/:todoId/items/:todoItemId", todoItemControllers.update)
+    app.delete("/api/todos/:todoId/items/:todoItemId", todoItemControllers.destroy)
+
     app.get("/api/todos/:todoId", todoControllers.retrieve);
     app.put("/api/todos/:todoId", todoControllers.update);
     app.delete("/api/todos/:todoId", todoControllers.destroy)
     app.post("/api/todos", todoControllers.create);
     app.get("/api/todos", todoControllers.list);
+
+    app.all("/api/todos/:todoId/items", (req, res)=>{
+        res.status(405).send({
+            message:"Operation not allowed"
+        })
+    })
 
 }
